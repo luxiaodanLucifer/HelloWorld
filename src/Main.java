0f8501d9c0dc7314fdc74a3defc46b38cec59d6e
lucifer <781562184@qq.com>
2017-02-22 10:40:06 +0800

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        File file = new File("aaa");
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
